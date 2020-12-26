7bfdfdf17e7fbc8fea72b7eb08e718f812b842b3
Eduardo <eduardofcbg@gmail.com>
2020-12-26 23:58:26 +0000

    first commit

---
+++ src/fetch-observable.ts
@@ -0,0 +1,80 @@
+import { observable, action, _allowStateChanges } from "mobx"
+
+type Fetch<T> = (sink: (newValue: T) => void) => void
+
+export interface IFetchObservable<T> {
+    current(): T | undefined
+    fetch(): T | undefined
+    set(value: T): T | undefined
+    setFetch(newFetch: Fetch<T>): T | undefined
+    pending: boolean
+    started: boolean
+}
+
+export function fetchObservable<T>(fetch: Fetch<T>): IFetchObservable<T | undefined>
+export function fetchObservable(): IFetchObservable<undefined>
+
+export function fetchObservable<T>(
+    initialFetch: Fetch<T> | undefined = undefined
+): IFetchObservable<T> {
+    let started = false
+
+    const initialValue = undefined
+    const emptyFetch: Fetch<undefined> = (sink) => {
+        sink(initialValue)
+    }
+
+    const fetch = observable.box<Fetch<T | undefined>>(initialFetch || emptyFetch, { deep: false })
+    const value = observable.box<T | undefined>(initialValue, { deep: false })
+    const pending = observable.box<boolean>(false)
+
+    const currentFnc = () => {
+        if (!started) {
+            started = true
+            _allowStateChanges(true, () => {
+                pending.set(true)
+            })
+
+            const currentFetch = fetch.get()
+            currentFetch((newValue) => {
+                _allowStateChanges(true, () => {
+                    value.set(newValue)
+                    pending.set(false)
+                })
+            })
+        }
+
+        return value.get()
+    }
+
+    const setFnc = action("lazyObservable-set", (newValue: T) => {
+        value.set(newValue)
+        pending.set(false)
+        return value.get()
+    })
+
+    const setFetchFnc = action("lazyObservable-setFetch", (newFetch: Fetch<T>): T | undefined => {
+        fetch.set(newFetch)
+        return value.get()
+    })
+
+    return {
+        current: currentFnc,
+        fetch() {
+            if (started) started = false
+            return currentFnc()
+        },
+        set(newVal: T) {
+            return setFnc(newVal)
+        },
+        setFetch(newFetch: Fetch<T>): T | undefined {
+            return setFetchFnc(newFetch)
+        },
+        get pending() {
+            return pending.get()
+        },
+        get started() {
+            return started && fetch.get() !== emptyFetch
+        },
+    }
+}
---
+++ test/fetch-observable.js
@@ -0,0 +1,271 @@
+"use strict"
+
+const { fetchObservable } = require("../src/fetch-observable")
+const mobx = require("mobx")
+
+mobx.configure({ enforceActions: "observed" })
+
+test("fetch observable should be lazy", (done) => {
+    let started = false
+    const fo = fetchObservable((sink) => {
+        started = true
+
+        setTimeout(() => sink(1), 50)
+        setTimeout(() => sink(2), 100)
+    })
+
+    expect(started).toBe(false)
+    expect(fo.started).toBe(false)
+
+    const values = []
+
+    mobx.autorun(() => {
+        const value = fo.current()
+        values.push(value)
+    })
+
+    expect(started).toBe(true)
+    expect(fo.started).toBe(true)
+
+    setTimeout(() => {
+        expect(fo.current()).toBe(2)
+        expect(values).toEqual([undefined, 1, 2])
+
+        done()
+    }, 200)
+})
+
+test("fetch observable sync", (done) => {
+    const fo = fetchObservable((sink) => {
+        sink(1)
+    })
+
+    const values = []
+    const pendingValues = []
+    const startedValues = []
+
+    mobx.autorun(() => {
+        const value = fo.current()
+
+        values.push(value)
+        pendingValues.push(fo.pending)
+        startedValues.push(fo.started)
+    })
+
+    expect(fo.started).toBe(true)
+
+    setTimeout(() => {
+        expect(fo.current()).toBe(1)
+        expect(values).toEqual([1])
+        expect(pendingValues).toEqual([false])
+        expect(startedValues).toEqual([true])
+
+        done()
+    }, 200)
+})
+
+test("fetch observable set", (done) => {
+    const fo = fetchObservable((sink) =>
+        setTimeout(() => {
+            sink(1)
+        }, 200)
+    )
+
+    const values = []
+    const pendingValues = []
+
+    mobx.autorun(() => {
+        const value = fo.current()
+
+        values.push(value)
+        pendingValues.push(fo.pending)
+    })
+
+    setTimeout(() => {
+        fo.set(2)
+    }, 100)
+
+    setTimeout(() => {
+        expect(fo.current()).toBe(1)
+        expect(values).toEqual([undefined, 2, 1])
+        expect(pendingValues).toEqual([true, false, false])
+
+        done()
+    }, 300)
+})
+
+test("fetch observable pending can be observed", (done) => {
+    const fo = fetchObservable((sink) =>
+        setTimeout(() => {
+            sink(1)
+        }, 100)
+    )
+
+    const values = []
+    const pendingValues = []
+
+    mobx.autorun(() => {
+        const value = fo.current()
+
+        values.push(value)
+        pendingValues.push(fo.pending)
+    })
+
+    setTimeout(() => {
+        expect(values).toEqual([undefined, 1, 1])
+        expect(pendingValues).toEqual([true, true, false])
+
+        done()
+    }, 200)
+})
+
+test("lazy observable fetch", (done) => {
+    let started = 0
+    let i = 10
+
+    const fo = fetchObservable((sink) => {
+        setTimeout(() => {
+            started++
+            i++
+
+            sink(i)
+        }, 50)
+    })
+
+    let values = []
+    mobx.autorun(() => values.push(fo.current()))
+
+    setTimeout(() => {
+        expect(started).toBe(1)
+        expect(fo.current()).toBe(11)
+        expect(values).toEqual([undefined, 11])
+
+        fo.fetch()
+    }, 100)
+
+    setTimeout(() => {
+        expect(started).toBe(2)
+        expect(fo.current()).toBe(12)
+        expect(values).toEqual([undefined, 11, 12])
+        done()
+    }, 200)
+})
+
+test("fetch observable is unstarted", (done) => {
+    const fo = fetchObservable()
+
+    const values = []
+    const startValues = []
+
+    mobx.autorun(() => {
+        const value = fo.current()
+
+        values.push(value)
+        startValues.push(fo.started)
+    })
+
+    setTimeout(() => {
+        expect(fo.current()).toBe(undefined)
+        expect(fo.started).toBe(false)
+        expect(values).toEqual([undefined])
+        expect(startValues).toEqual([false])
+
+        done()
+    }, 200)
+})
+
+test("unstarted fetch observable should fetch", (done) => {
+    const fo = fetchObservable()
+
+    const values = []
+    const startValues = []
+
+    mobx.autorun(() => {
+        const value = fo.current()
+
+        values.push(value)
+        startValues.push(fo.started)
+    })
+
+    fo.setFetch((sink) => {
+        setTimeout(() => sink(1), 50)
+    })
+
+    expect(fo.current()).toBe(undefined)
+
+    setTimeout(() => {
+        expect(fo.current()).toBe(undefined)
+
+        fo.fetch()
+    }, 100)
+
+    setTimeout(() => {
+        expect(fo.current()).toBe(1)
+        expect(values).toEqual([undefined, undefined, 1])
+        expect(startValues).toEqual([false, true, true])
+
+        done()
+    }, 200)
+})
+
+test("fetch observable pending can be observed on setFetch", (done) => {
+    const fo = fetchObservable((sink) =>
+        setTimeout(() => {
+            sink(1)
+        }, 100)
+    )
+
+    const values = []
+    const pendingValues = []
+
+    mobx.autorun(() => {
+        const value = fo.current()
+
+        values.push(value)
+        pendingValues.push(fo.pending)
+    })
+
+    setTimeout(() => {
+        fo.setFetch((sink) => {
+            setTimeout(() => {
+                sink(2)
+            }, 100)
+        })
+
+        fo.fetch()
+    }, 200)
+
+    setTimeout(() => {
+        expect(values).toEqual([undefined, 1, 1, 1, 2, 2])
+        expect(pendingValues).toEqual([true, true, false, true, true, false])
+
+        done()
+    }, 600)
+})
+
+test("started fetch observable should fetch", (done) => {
+    const fo = fetchObservable((sink) => setTimeout(() => sink(1), 100))
+
+    const values = []
+
+    mobx.autorun(() => {
+        const value = fo.current()
+
+        values.push(value)
+    })
+
+    setTimeout(() => {
+        fo.setFetch((sink) => {
+            sink(2)
+        })
+
+        fo.fetch()
+    }, 200)
+
+    setTimeout(() => {
+        expect(fo.current()).toBe(2)
+        expect(values).toEqual([undefined, 1, 2])
+
+        done()
+    }, 300)
+})
